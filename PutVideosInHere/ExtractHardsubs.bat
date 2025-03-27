ed801700b3ffc0132fc4527375d22e3a376b8769
Wyatt Kaliser <wyattpetrock@gmail.com>
2025-03-26 23:31:22 -0400

    Update ExtractHardsubs.bat

--- PutVideosInHere/ExtractHardsubs.bat
+++ PutVideosInHere/ExtractHardsubs.bat
@@ -2,35 +2,32 @@
 setlocal enabledelayedexpansion
 
 REM Set crop values, default (for Muse) L 0.15, R 0.85, T 0.19, B 0.02
-set CROP_LEFT=0.15
-set CROP_RIGHT=0.85
-set CROP_TOP=0.20
-set CROP_BOTTOM=0.05
+set CROP_LEFT=0.08
+set CROP_RIGHT=0.92
+set CROP_TOP=0.24
+set CROP_BOTTOM=0.07
 
-REM Set the path to the VideoSubFinder executable
+REM Set the path to VideoSubFinder
 set VIDEOSUBFINDER_PATH="..\Release_x64\VideoSubFinderWXW.exe"
 
-REM Set the path to the folder containing the videos
+REM Set the video folder path
 set VIDEOS_FOLDER=.
 
-REM Set the path to the output folder in the working directory
+REM Set output folder
 set OUTPUT_FOLDER=%VIDEOS_FOLDER%\output
-
-REM Ensure the output folder exists
 if not exist %OUTPUT_FOLDER% (
     mkdir %OUTPUT_FOLDER%
 )
 
-REM Iterate over each video file in the videos folder with .mp4 or .mkv extension
+REM Process each video file
 for %%f in ("%VIDEOS_FOLDER%\*.mp4" "%VIDEOS_FOLDER%\*.mkv") do (
     echo Processing %%~nxf
     set VIDEO_FILE="%%f"
     set OUTPUT_FILE="%OUTPUT_FOLDER%\%%~nf"
     
-    REM Run VideoSubFinder CLI for the current video file, using the crop values
-    start /wait "" %VIDEOSUBFINDER_PATH% -c -r -ccti -i !VIDEO_FILE! -o !OUTPUT_FILE! -te %CROP_TOP% -be %CROP_BOTTOM% -le %CROP_LEFT% -re %CROP_RIGHT% -s 0:00:0:300 --use_cuda
-   
-    REM Check if the command succeeded
+    REM Run VideoSubFinder with binarization adjustments
+    start /wait "" %VIDEOSUBFINDER_PATH% -c -r -ccti -i !VIDEO_FILE! -o !OUTPUT_FILE! -te %CROP_TOP% -be %CROP_BOTTOM% -le %CROP_LEFT% -re %CROP_RIGHT% -s 0:00:0:300 --use_cuda     
+    REM Check success
     if errorlevel 1 (
         echo Failed to process !VIDEO_FILE!
     ) else (
